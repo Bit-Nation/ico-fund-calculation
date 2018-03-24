675ad31fbf4bf0240a79a818791da54e75f19de7
Florian Lenz <flenz81@googlemail.com>
2018-03-24 13:46:51 +0100

    init project

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# ICO collected funds microservices
+
+This is a microservices used to calculate our ico funds.
---
+++ index.js
@@ -0,0 +1,60 @@
+//Deps
+const express = require('express');
+const app = express();
+const ethers = require('ethers');
+const BigNumber = require('big.js');
+const winston = require('winston');
+require('winston-papertrail').Papertrail;
+
+const SALE_ADDRESS = process.env.SALE_ADDRESS;
+const MULTISIG_ADDRESS = process.env.MULTISIG_ADDRESS;
+const JSON_RPC_URL = process.env.JSON_RPC_URL;
+
+const ethersProvider = new ethers.providers.JsonRpcProvider(JSON_RPC_URL);
+
+//Logger
+const logger = new winston.Logger({
+    transports: [
+        new winston.transports.Papertrail({
+            host: process.env.PAPERTRAIL_HOST,
+            port: process.env.PAPERTRAIL_PORT,
+            colorize: true,
+            hostname: process.env.PAPERTRAIL_HOST_NAME
+        })
+    ]
+});
+
+//Catch errors
+app.use(function (err, req, res, next) {
+    logger.error(err);
+    res.status(500);
+    res.send();
+});
+
+//Calc collected funds
+app.get('/', function (req, res) {
+
+    let investedEth;
+
+	ethersProvider
+        .getBalance(SALE_ADDRESS)
+        .then(function (amount) {
+        	investedEth = amount;
+        	return ethersProvider.getBalance(MULTISIG_ADDRESS);
+        })
+        .then(function (amount) {
+            investedEth = investedEth.add(amount);
+            investedEth = investedEth.toString(10);
+            investedEth = new BigNumber(ethers.utils.formatUnits(investedEth, 'ether'));
+
+            res.send({
+                collected_eth: investedEth.toFixed(3).toString()
+            })
+        })
+        .catch(console.error)
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("started server")
+});
